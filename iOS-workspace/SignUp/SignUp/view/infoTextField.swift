0461089afab16d130da8a28f41b6b40e5b887923
limwin94 <amuguna94@gmail.com>
2020-03-24 16:29:17 +0900

    [#15] style: placeholder 및 입력시 left, right padding 추가

---
+++ iOS-workspace/SignUp/SignUp/view/infoTextField.swift
@@ -0,0 +1,26 @@
+//
+//  infoTextField.swift
+//  SignUp
+//
+//  Created by 임승혁 on 2020/03/24.
+//  Copyright © 2020 임승혁. All rights reserved.
+//
+
+import UIKit
+
+class infoTextField: UITextField {
+
+    let padding = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 5)
+
+    override open func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+
+    override open func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+
+    override open func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+}
