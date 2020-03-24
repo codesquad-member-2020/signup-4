dce3b21eac7690dca4cbbbd15b5f40634190a68c
baekCode <sosaer88@gmail.com>
2020-03-25 01:14:22 +0900

    [#13] feat: 입력 케이스

--- FE-workspace/src/js/register.js
+++ FE-workspace/src/js/register.js
@@ -5,31 +5,67 @@ import {
   mockData
 } from "../util/constant.js";
 import { $select, $selectAll, $getByID, $addListener } from "../util/func.js";
-import { printMessage, addClassMessage, removeClassMessage } from './infoMessage.js'
+import {
+  printMessage,
+  addClassMessage,
+  removeClassMessage
+} from "./infoMessage.js";
 
 const regEl = $select(".register");
 
+// 상수를 사용하여도 순수함수가 될 수 있다. 상수는 변화가 없기 때문에..
 const check = (reg, el, successMsg, errorMsg) => {
   if (reg.test(el.value)) {
-    removeClassMessage(el,'error');
+    removeClassMessage(el, "error");
     return printMessage(successMsg, el);
   }
-  addClassMessage(el,'error');
+  addClassMessage(el, "error");
   printMessage(errorMsg, el);
   el.value = "";
   el.focus();
 };
 
 const changeCallback = event => {
-  if (
-    !check(
-      REG_PATTERN.userID,
-      event.target,
-      INFO_MESSAGE.userID.successMsg,
-      INFO_MESSAGE.userID.errorMsg
-    )
-  )
-    return false;
+  switch (event.target.id) {
+    case "userID":
+      if (!check(REG_PATTERN.userID,event.target,INFO_MESSAGE.userID.successMsg,INFO_MESSAGE.userID.errorMsg)) return false;
+      break;
+    case "password":
+      if (!check(REG_PATTERN.password,event.target,INFO_MESSAGE.password.successMsg,INFO_MESSAGE.password.errorMsg.limit)) return false;
+      break;
+    case "password_confirm":
+      if (!check(REG_PATTERN.password,event.target,INFO_MESSAGE.password.successMsg,INFO_MESSAGE.password.errorMsg.limit)) return false;
+      break;
+    case "birthYY":
+      console.log('YY')
+      break;
+    case "birthMM":
+      console.log('MM')
+      break;
+    case "birthDD":
+      console.log('DD')
+      break;
+    case "email":
+      console.log('email')
+      break;
+    case "mobile":
+      console.log('mobile')
+      break;
+    case "interest":
+      console.log('관심사')
+      break;
+    default:
+      break;
+  }
+  // if (
+  //   !check(
+  //     REG_PATTERN.userID,
+  //     event.target,
+  //     INFO_MESSAGE.userID.successMsg,
+  //     INFO_MESSAGE.userID.errorMsg
+  //   )
+  // )
+  //   return false;
 };
 
 $addListener(document, "DOMContentLoaded", () => {
