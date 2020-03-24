5a6d9d7cb4e5728cfb7fb72c097b98bc8fae9822
baekCode <sosaer88@gmail.com>
2020-03-24 13:30:00 +0900

    [#12] chore: 필드값 수정 및 mock data

---
+++ FE-workspace/src/util/constant.js
@@ -0,0 +1,33 @@
+export const mockData = {
+  user: [
+    {
+      userId: "baekco",
+      email: "baekco@google.com",
+      birthDate: "2020-01-01",
+      mobile: "010-5555-5555",
+      sex: "여자",
+      interest: ["웹툰", "쇼핑", "축구"]
+    },{
+      userId: "sigrid",
+      email: "sigrid@google.com",
+      birthDate: "2020-01-01",
+      mobile: "010-5555-5555",
+      sex: "여자",
+      interest: ["웹툰", "쇼핑", "축구"]
+    },{
+      userId: "ellie",
+      email: "ellie@google.com",
+      birthDate: "2020-01-01",
+      mobile: "010-5555-5555",
+      sex: "여자",
+      interest: ["웹툰", "쇼핑", "축구"]
+    },{
+      userId: "Lin",
+      email: "Lin@google.com",
+      birthDate: "2020-01-01",
+      mobile: "010-5555-5555",
+      sex: "여자",
+      interest: ["웹툰", "쇼핑", "축구"]
+    }
+  ]
+};
