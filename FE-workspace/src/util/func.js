ab9effb37ab29d46745494684dcae11145fc2686
baekCode <sosaer88@gmail.com>
2020-03-24 12:39:20 +0900

    [#12] feat: FE setup 및 회원가입 html/css

---
+++ FE-workspace/src/util/func.js
@@ -0,0 +1,3 @@
+export const $select = el => document.querySelector(el);
+export const $selectAll = el => document.querySelectorAll(el);
+export const $addListener = (target, type, callback) => target.addEventListener(type, callback);
