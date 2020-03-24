b1f8ae2dd3114fba0d8b69a43f436a3df15b5a4e
limwin94 <amuguna94@gmail.com>
2020-03-24 14:55:06 +0900

    [#15] style: add sign up view (skeleton)
    not realize feature

---
+++ iOS-workspace/SignUp/SignUp/view/TitleLabel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleLabel.swift
+//  SignUp
+//
+//  Created by 임승혁 on 2020/03/24.
+//  Copyright © 2020 임승혁. All rights reserved.
+//
+
+import UIKit
+
+class TitleLabel: UILabel {
+    
+}
