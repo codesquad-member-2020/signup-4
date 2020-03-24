c5127fb00ee85585778693775e2b9486014a0b74
limwin94 <amuguna94@gmail.com>
2020-03-24 16:34:44 +0900

    [#15] refactor: 쓰지 않는 클래스 삭제(StackView), 주석 제거

--- iOS-workspace/SignUp/SignUp/SignUpViewController.swift
+++ iOS-workspace/SignUp/SignUp/SignUpViewController.swift
@@ -12,7 +12,7 @@ class SignUpViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
