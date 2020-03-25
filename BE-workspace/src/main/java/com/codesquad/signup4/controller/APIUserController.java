3da3a81b40ab3c4920e83a843ec97aca43e1b95b
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-03-25 11:19:42 +0900

    feat: Jackson 적용, JSON 데이터 출력 및 입력 시 받도록 테스트 제작 - User class의 feature가 정상적으로 생성되지 않아 테스트 실행 시 에러

--- BE-workspace/src/main/java/com/codesquad/signup4/controller/APIUserController.java
+++ BE-workspace/src/main/java/com/codesquad/signup4/controller/APIUserController.java
@@ -4,47 +4,60 @@ import com.codesquad.signup4.domain.User;
 import com.codesquad.signup4.domain.UserRepository;
 import com.codesquad.signup4.dto.Result;
 import com.codesquad.signup4.exception.BadRequestException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
 public class APIUserController {
-    @Autowired
-    UserRepository userRepository;
 
-    @PostMapping("/create")
-    public Result create(User newUser) {
+  @Autowired
+  UserRepository userRepository;
+
+  ObjectMapper objectMapper = new ObjectMapper();
+
+  @PostMapping("/create")
+  public String create(User newUser) throws JsonProcessingException {
 //        Todo 일단 이렇게 작성만 해놓음
-        if(newUser.getUserId() == null) {
-            throw new BadRequestException();
-        }
-        return Result.ok();
+    if (newUser.getUserId() == null) {
+      throw new BadRequestException();
+    }
+    return okResultProvider();
+  }
+
+  @GetMapping("/checkId")
+  public String checkId(@RequestParam String id) throws JsonProcessingException {
+    if (id == null) {
+      throw new BadRequestException();
     }
 
-    @GetMapping("/checkId")
-    public Result checkId(@RequestParam String id) {
-        if(id == null) {
-            throw new BadRequestException();
-        }
+    if (id.equals("testUserId")) {
+      return failResultProvider("중복된 아이디입니다.");
+    }
+    return okResultProvider();
+  }
 
-        if(id.equals("testUserId")) {
-            return Result.fail("중복된 아이디입니다.");
-        }
-
-        return Result.ok();
+  @GetMapping("/checkMobile")
+  public String checkMobile(@RequestParam String mobileNumber) throws JsonProcessingException {
+    if (mobileNumber == null) {
+      throw new BadRequestException();
     }
 
-    @GetMapping("/checkMobile")
-    public Result checkMobile(@RequestParam String mobileNumber) {
-        if(mobileNumber == null) {
-            throw new BadRequestException();
-        }
-
-        if(mobileNumber.equals("01012341234")) {
-            return Result.fail("중복된 번호입니다.");
-        }
-
-        return Result.ok();
+    if (mobileNumber.equals("01012341234")) {
+        return failResultProvider("중복된 번호입니다.");
     }
+    return okResultProvider();
+  }
+
+  public String okResultProvider() throws JsonProcessingException {
+    Result okResult = Result.ok();
+    return objectMapper.writeValueAsString(okResult);
+  }
+
+  public String failResultProvider(String errorMessage) throws JsonProcessingException {
+      Result failResult = Result.fail(errorMessage);
+      return objectMapper.writeValueAsString(failResult);
+  }
 }
