538bc79342d6c4af04b7b2e7ab3480c9723ee3ea
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-03-25 20:11:24 +0900

    chore: 로그인한 사용자에 대한 상세 정보를 JSON 데이터에 담아 리턴하는 컨트롤러 구현 - 현재 테스트 API 용으로 개발되어 자바지기의 데이터를 미리 실행 전에 넣고, 이것만 반환받을 수 있음 - 로그인하지 않은 사용자에 대한 unauthorized 응답은 현재 구현하지 못함

--- BE-workspace/src/main/java/com/codesquad/signup4/controller/userapi/APIUserDetailController.java
+++ BE-workspace/src/main/java/com/codesquad/signup4/controller/userapi/APIUserDetailController.java
@@ -1,5 +1,42 @@
 package com.codesquad.signup4.controller.userapi;
 
+import com.codesquad.signup4.domain.Interest;
+import com.codesquad.signup4.domain.User;
+import com.codesquad.signup4.domain.UserRepository;
+import com.codesquad.signup4.dto.UserInfoDto;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
 public class APIUserDetailController {
 
+  @Autowired
+  private UserRepository userRepository;
+
+  private void insertTestUser() {
+    User javajigi = User
+        .create("javajigi", "password", "javajigi@gmail.com", "여자", "testUserName", "2020-01-01",
+            "01012341234");
+    List<Interest> interest = new ArrayList<>();
+    interest.add(new Interest("soccer"));
+    interest.add(new Interest("shopping"));
+    javajigi.addInterest(interest);
+    userRepository.save(javajigi);
+  }
+
+  @GetMapping("/checkmyinfo")
+  public UserInfoDto checkMyInfo(HttpSession httpSession) {
+    insertTestUser();
+    User user = (User) userRepository.findAll();
+    return UserInfoDto.createUserInfoDto(
+            user.getUserID(), user.getPassword(), user.getEmail(), user.getGender(),
+            user.getUserName(), user.getBirthDate(), user.getMobile(), user.getInterest()
+    );
+  }
 }
