f8bd0aa3d00cc54a9878e89cbd05ec4cfd1c85a5
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-03-25 13:21:27 +0900

    chore: TestAPI에서 공유하는 Result 클래스의 JSON serializer 객체 분리

--- BE-workspace/src/main/java/com/codesquad/signup4/controller/testdummydata/TestDummyDataUtils.java
+++ BE-workspace/src/main/java/com/codesquad/signup4/controller/testdummydata/TestDummyDataUtils.java
@@ -1,5 +1,19 @@
 package com.codesquad.signup4.controller.testdummydata;
 
-public class TestDummyDataUtils {
+import com.codesquad.signup4.dto.Result;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+public class TestDummyDataUtils {
+  static ObjectMapper objectMapper = new ObjectMapper();
+
+  public static String okResultProvider() throws JsonProcessingException {
+    Result okResult = Result.ok();
+    return objectMapper.writeValueAsString(okResult);
+  }
+
+  public static String failResultProvider(String errorMessage) throws JsonProcessingException {
+    Result failResult = Result.fail(errorMessage);
+    return objectMapper.writeValueAsString(failResult);
+  }
 }
