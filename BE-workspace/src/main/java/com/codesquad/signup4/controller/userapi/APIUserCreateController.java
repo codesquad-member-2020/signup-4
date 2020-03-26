94a017b47b4950528947f6ae1e899621db0ebd4a
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-03-26 09:49:54 +0900

    chore: 중복 아이디, 이메일, 핸드폰 번호를 체크 로직을 중복 제거함

--- BE-workspace/src/main/java/com/codesquad/signup4/controller/userapi/APIUserCreateController.java
+++ BE-workspace/src/main/java/com/codesquad/signup4/controller/userapi/APIUserCreateController.java
@@ -14,59 +14,63 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/users")
 public class APIUserCreateController {
 
-    private static final Logger log = LoggerFactory.getLogger(APIUserCreateController.class);
+  private static final Logger log = LoggerFactory.getLogger(APIUserCreateController.class);
 
-    @Autowired
-    UserRepository userRepository;
+  @Autowired
+  UserRepository userRepository;
 
-    @InitBinder
-    public void initBinder(WebDataBinder binder) {
-        binder.initDirectFieldAccess();
+  @InitBinder
+  public void initBinder(WebDataBinder binder) {
+    binder.initDirectFieldAccess();
+  }
+
+  @PostMapping("/create")
+  public Result create(User newUser) {
+    if (!newUser.isValid()) {
+      throw new BadRequestException();
     }
 
-    @PostMapping("/create")
-    public Result create(User newUser) {
-        if (!newUser.isValid()) {
-            throw new BadRequestException();
-        }
+    return Result.ok();
+  }
 
-        return Result.ok();
+  @GetMapping("/duplicate/checkID")
+  public Result checkId(@RequestParam String id) {
+
+    checkNull(id);
+
+    if (userRepository.findByUserID(id) != null) {
+      return Result.fail("중복된 아이디입니다.");
     }
 
-    @GetMapping("/duplicate/checkID")
-    public Result checkId(@RequestParam String id) {
-        if (id == null || id.equals("")) {
-            throw new BadRequestException();
-        }
+    return Result.ok();
+  }
 
-        if (userRepository.findByUserID(id) != null) {
-            return Result.fail("중복된 아이디입니다.");
-        }
+  @GetMapping("/duplicate/checkEmail")
+  public Result checkEmail(@RequestParam String email) {
 
-        return Result.ok();
+    checkNull(email);
+
+    if (userRepository.findByEmail(email) != null) {
+      return Result.fail("중복된 이메일입니다.");
     }
+    return Result.ok();
+  }
 
-    @GetMapping("/duplicate/checkEmail")
-    public Result checkEmail(@RequestParam String email) {
-        if (email == null || email.equals("")) {
-            throw new BadRequestException();
-        }
-        if (userRepository.findByEmail(email) != null) {
-            return Result.fail("중복된 이메일입니다.");
-        }
-        return Result.ok();
+  @GetMapping("/duplicate/checkMobile")
+  public Result checkMobile(@RequestParam String mobileNumber) {
+
+    checkNull(mobileNumber);
+
+    if (userRepository.findByMobile(mobileNumber) != null) {
+      return Result.fail("중복된 번호입니다.");
     }
+    return Result.ok();
+  }
 
-    @GetMapping("/duplicate/checkMobile")
-    public Result checkMobile(@RequestParam String mobileNumber) {
+  public void checkNull(String auditee) {
 
-        if (mobileNumber == null || mobileNumber.equals("")) {
-            throw new BadRequestException();
-        }
-
-        if (userRepository.findByMobile(mobileNumber) != null) {
-            return Result.fail("중복된 번호입니다.");
-        }
-        return Result.ok();
+    if (auditee == null || auditee.equals("")) {
+      throw new BadRequestException();
     }
+  }
 }
