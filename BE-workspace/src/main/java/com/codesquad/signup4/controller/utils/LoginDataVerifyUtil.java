2b73fff990a360385fb28af8e6a796b5d19389db
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-03-26 22:20:34 +0900

    feat: 로그인 아이디 및 비밀번호 형식에 맞는지 체크하는 메소드

--- BE-workspace/src/main/java/com/codesquad/signup4/controller/utils/LoginDataVerifyUtil.java
+++ BE-workspace/src/main/java/com/codesquad/signup4/controller/utils/LoginDataVerifyUtil.java
@@ -1,5 +1,27 @@
 package com.codesquad.signup4.controller.utils;
 
-public class LoginVerifyUtil {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class LoginDataVerifyUtil {
+
+  public static boolean checkUserIDformat(String userID) {
+    String checkFormat = "(?i)^(?=.*[a-z])[a-z0-9]{8,20}$";
+    return checkFormatAuditor(checkFormat, userID);
+  }
+
+  public static boolean checkUserPasswordformat(String password) {
+    String checkFormat = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,16}$";
+
+    return checkFormatAuditor(checkFormat, password);
+  }
+
+  public static boolean checkFormatAuditor(String pattern, String auditee) {
+    Pattern password = Pattern.compile(pattern);
+    Matcher matcher = password.matcher(auditee);
+    if (!matcher.find()) {
+      return false;
+    }
+    return matcher.group().equals(auditee);
+  }
 }
