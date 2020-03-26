d294d0127dd084718d659664c19538c575008311
baekCode <sosaer88@gmail.com>
2020-03-26 13:29:57 +0900

    [#13] feat: 유저데이터 초기화

---
+++ FE-workspace/src/js/registerUserData.js
@@ -0,0 +1,14 @@
+export const STATE_USER_DATA = {
+  userID: null,
+  password: null,
+  password_confirm: null,
+  userName: null,
+  birthYY: null,
+  birthMM: null,
+  birthDD: null,
+  gender: null,
+  email: null,
+  mobile: null,
+  interest: null,
+  agree: null
+};
