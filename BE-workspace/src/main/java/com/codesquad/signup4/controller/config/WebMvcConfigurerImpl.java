6ff211a422779876a90d9473a690aa851782bdc4
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-03-27 17:24:11 +0900

    chore: WebMvcConfigurer implementation to solve CORS error

---
+++ BE-workspace/src/main/java/com/codesquad/signup4/controller/config/WebMvcConfigurerImpl.java
@@ -0,0 +1,14 @@
+package com.codesquad.signup4.controller.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfigurerImpl implements WebMvcConfigurer {
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedMethods("GET", "POST");
+  }
+}
