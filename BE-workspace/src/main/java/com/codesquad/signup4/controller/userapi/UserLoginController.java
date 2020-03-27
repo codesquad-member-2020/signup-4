35d54f6557daee9a8419b3cbff4e5636f4f10649
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-03-27 14:14:35 +0900

    chore: import 문제 해결

--- BE-workspace/src/main/java/com/codesquad/signup4/controller/userapi/UserLoginController.java
+++ BE-workspace/src/main/java/com/codesquad/signup4/controller/userapi/UserLoginController.java
@@ -1,11 +1,21 @@
 package com.codesquad.signup4.controller.userapi;
 
+import com.codesquad.signup4.controller.utils.HttpSessionUtil;
+import com.codesquad.signup4.controller.utils.LoginDataVerifyUtil;
 import com.codesquad.signup4.domain.Interest;
 import com.codesquad.signup4.domain.User;
 import com.codesquad.signup4.domain.UserRepository;
 import com.codesquad.signup4.dto.Result;
+import com.codesquad.signup4.exception.PasswordFormatException;
+import com.codesquad.signup4.exception.UnauthorizedException;
+import com.codesquad.signup4.exception.UserNotFoundException;
+import com.codesquad.signup4.exception.userIDFormatException;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +26,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/auth")
 public class UserLoginController {
+
+  @Autowired
+  private UserRepository userRepository;
 
   @PostMapping("/login")
   public ResponseEntity<Result> login(@RequestBody Map<String, String> userData, HttpSession httpSession) {
